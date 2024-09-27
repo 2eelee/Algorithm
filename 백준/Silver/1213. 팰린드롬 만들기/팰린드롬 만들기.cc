6d41d40a3a7ccb219c9467ecff70ec81a07abbea
유현서 <108321969+2eelee@users.noreply.github.com>
2024-09-28 08:52:31 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -1,114 +1,44 @@
 #include <iostream>
-#include <string>
 #include <vector>
-#include <iterator>
 #include <algorithm>
 
 using namespace std;
-const int NUM_CHARS = 26;
 
-void countFreq(string word, vector<int> &freq) {
-    
-    for (int i = 0; i < word.length(); i++) {
-        
-        freq[word[i] - 'A']++; 
+vector<int> cntAlphabet(string name){
+    vector<int> cnt(26, 0);
+    for(int i=0; i<name.size(); i++){
+        cnt[name[i]-'A']++;
     }
-
+    return cnt;
 }
 
-string getString(vector<int>& freq) {
-    string result;
-    for (int i = 0; i < freq.size(); ++i) {
-        
-        if(freq[i]>=2){
-            char ch = 'A' + i;                      // 해당 인덱스에 대응하는 알파벳
-            result += string(freq[i]/2, ch);  
-        }
-    }
-    return result;
-}
-
-bool checkPalindrome(vector<int> freq){
+string makePalindrome(vector<int>& cnt){
+    string str1="", str2="", str3="";
     
-    bool flag = false;
-    
-    for(auto num:freq){
-        num = num%2;
-        
-        if(num==1){
-            
-            if(flag==true){
-                return false;
+    for(int i=0; i<26; i++){
+        if(cnt[i]%2==1){
+            if(str2.size()==1){
+                return "I'm Sorry Hansoo";
             }
-            
-            flag=true;
+            str2=(char)(i+'A');
         }
-        
-    }
-    return true;
-}
-
-int findOddFreqIndex(vector<int> freq){
-    
-    //freq는 팰린드롬 가능한 문자열의 알파벳 빈도 수를 저장한 vector임을 가정
-    
-    for (int i = 0; i < freq.size(); ++i) {
-        
-        freq[i] = freq[i]%2;
-        
-        if (freq[i] == 1) {
-            return i; 
+        for(int j=0; j<cnt[i]/2; j++){
+            str1 += (char)(i+'A');
+            str3 = (char)(i+'A')+str3;
         }
     }
-    
-    return -1;
+    return str1 + str2 + str3;
 }
 
-string reverseString(string& input) {
-    string reversed;
-    // 입력된 문자열을 끝에서부터 시작해서 거꾸로 저장
-    for (int i = input.length() - 1; i >= 0; --i) {
-        reversed.push_back(input[i]);
-    }
-    return reversed;
+string solution(string name){
+    
+    vector<int> cnt = cntAlphabet(name);
+    return makePalindrome(cnt);
 }
 
-string makePalindrome(vector<int> freq){
-    
-    int index = findOddFreqIndex(freq);
-    string word = getString(freq);
-    string reverse = reverseString(word);
-    
-    if(index != -1){   // 짝수인 알파벳들로만 이뤄져 있는 경우 
-        char odd_char = (char)(index+'A');
-        word.push_back(odd_char);
-        
-        
-    }
-    
-    return word+reverse;
-}
-
-
-
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    
+int main(){
     string name;
     cin >> name;
     
-    vector<int> name_freq(NUM_CHARS, 0);
-    countFreq(name, name_freq);
-    
-    if(!checkPalindrome(name_freq)){
-        cout << "I'm Sorry Hansoo"<<'\n';
-        return 0;
-    }
-    
-    //cout << (char)(( findOddFreqIndex(name_freq) )+'A');
-    cout << makePalindrome(name_freq) << '\n';
-    
+    cout << solution(name);
 }
