cc7fe87818565472081e4021c2b41607ecf5e888
유현서 <108321969+2eelee@users.noreply.github.com>
2024-09-28 12:08:02 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -1,44 +1,44 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-vector<int> cntAlphabet(string name){
-    vector<int> cnt(26, 0);
-    for(int i=0; i<name.size(); i++){
-        cnt[name[i]-'A']++;
-    }
-    return cnt;
+vector<int> readAlphabet(string& name) {
+	vector<int> cnt(26,0);
+	int len = name.length();
+	for(int i=0; i<len; i++) {
+		cnt[name[i]-'A']++;
+	}
+	return cnt;
 }
 
-string makePalindrome(vector<int>& cnt){
-    string str1="", str2="", str3="";
-    
-    for(int i=0; i<26; i++){
-        if(cnt[i]%2==1){
-            if(str2.size()==1){
-                return "I'm Sorry Hansoo";
-            }
-            str2=(char)(i+'A');
-        }
-        for(int j=0; j<cnt[i]/2; j++){
-            str1 += (char)(i+'A');
-            str3 = (char)(i+'A')+str3;
-        }
-    }
-    return str1 + str2 + str3;
+string makePalindrome(vector<int>& cnt) {
+	string str1="", str2="", str3="";
+	for(int i=0; i<26; i++) {
+		if(cnt[i]%2==1) {
+			if(str2.length()==1) {
+				return "I'm Sorry Hansoo";
+			}
+			else {
+				str2=(char)(i+'A');
+			}
+		}
+		for(int j=0; j<cnt[i]/2; j++) {
+			str1+=(char)(i+'A');
+			str3=(char)(i+'A')+str3;
+		}
+	}
+	return str1 + str2 + str3;
 }
 
-string solution(string name){
-    
-    vector<int> cnt = cntAlphabet(name);
-    return makePalindrome(cnt);
+string solution(string& name) {
+	vector<int> cnt = readAlphabet(name);
+	return makePalindrome(cnt);
 }
 
-int main(){
-    string name;
-    cin >> name;
-    
-    cout << solution(name);
+int main()
+{
+	string name;
+	cin >> name;
+	cout << solution(name);
 }
