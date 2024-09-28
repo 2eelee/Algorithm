212375948b9c218be3ca903d246d2c421388d9c8
유현서 <108321969+2eelee@users.noreply.github.com>
2024-09-28 09:09:46 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
@@ -1,34 +1,33 @@
 #include <string>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
 int solution(vector<vector<int>> board, vector<int> moves) {
     int answer = 0;
+    int height = board.size();
+    int moves_cnt = moves.size();
+    stack<int> basket;
     
-    vector<int> basket;
-    int move_cnt = moves.size();
-    
-    for(int i=0; i<move_cnt; i++){
-        // board[][moves[i]-1] 뽑기
-        
-        // 알맞은 인형 집어서 바구니에 넣기
-        int depth = board.size();
-        for(int j=0; j<depth; j++){
-            if(board[j][moves[i]-1] != 0){ // 가장 위에 있는 인형 찾았으면
-                basket.push_back(board[j][moves[i]-1]);
-                board[j][moves[i]-1] = 0;
-                break;
+    for(int i=0; i<moves_cnt; i++){
+        int pos = moves[i]-1;
+        for(int j=0; j<height; j++){
+            
+            if(!board[j][pos]){
+                continue;
             }
-        }
-        
-        // 인형이 같은 모양이면 터뜨리기
-        if (basket[basket.size() - 1] == basket[basket.size() - 2]){
-            basket.pop_back();
-            basket.pop_back();
-            answer+=2;
-        }
-    }
-    
+            int doll = board[j][pos];
+            board[j][pos]=0;
+            if(!basket.empty() && basket.top() == doll){
+                basket.pop();
+                answer+=2;
+            }
+            else{
+                basket.push(doll);
+            }
+            break;
+            }
+    }    
     return answer;
 }
