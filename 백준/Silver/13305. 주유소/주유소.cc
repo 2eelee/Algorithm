3c09e24b1b3a6abd0d85f2590769aab4a6bfaf6a
유현서 <108321969+2eelee@users.noreply.github.com>
2024-10-15 02:36:27 +0900

    [Silver III] Title: 주유소, Time: 12 ms, Memory: 2804 KB, Score: 58 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.cc
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -5,22 +5,17 @@ using namespace std;
 
 int minOilConst(int n, vector<int>& road_length, vector<int>& oil_cost){
     
-    int result = 0;
-    
     int total_cost = 0;
-    int min_cost = 10000;
-    int total_dist = 0;
+    int min_cost = oil_cost[0];
     
     for(int i=0; i<n-1; i++){
-        total_dist += road_length[i];
+        total_cost += min_cost*road_length[i];
         
-        if(oil_cost[i] < min_cost){
-            min_cost = oil_cost[i];
-            total_cost += min_cost * total_dist;
-            total_dist = 0;
+        if(oil_cost[i+1] < min_cost){
+            min_cost = oil_cost[i+1];
+            
         }
     }
-    total_cost += min_cost * total_dist;
     return total_cost;
 }
 
@@ -33,8 +28,8 @@ int main()
     int n;
     cin >> n;
     
-    vector<int> road_length(n-1,0);    // 0~n-2번 도로의 길이
-    vector<int> oil_cost(n,0);  // 0~n-1번 주유소의 기름값
+    vector<int> road_length(n-1,0);     // 0~n-2번 도로의 길이
+    vector<int> oil_cost(n,0);          // 0~n-1번 주유소의 기름값
     
     for(int i=0; i<n-1; i++){
         cin >> road_length[i];
